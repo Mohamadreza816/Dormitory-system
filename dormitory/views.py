419d4d1d7ebc0b22a97efb3ef91893bc88ef40fd
Mohamadreza816 <mohammadrezaheidarnia@gmail.com>
2025-10-29 18:43:03 +0330

    add api for dormitory name

--- dormitory/views.py
+++ dormitory/views.py
@@ -7,10 +7,22 @@ from rest_framework.views import APIView
 from .models import dormitory
 from .serializers import dormitory_name_serializer
 from allusers.permitions import StudentUser,AdminUser
+from drf_spectacular.utils import extend_schema
 # Create your views here.
 class dormitoryname(APIView):
     permission_classes = [AdminUser]
     serializer_class = dormitory_name_serializer
+    @extend_schema(
+        responses={
+            200: {
+                "description": "return name of dormitory",
+                "example": {
+                    "name": "ghadir",
+                    "Gender": "Male"
+                }
+            }
+        }
+    )
     def get(self,request):
         queryset = dormitory.objects.all()
         serializer = dormitory_name_serializer(queryset,many=True)
